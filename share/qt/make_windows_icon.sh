7cd141109104b71c3cf9cc041842dfcf46f1ef1f
Arcane Crypto <41708145+ArcaneCrypto@users.noreply.github.com>
2021-01-31 05:25:11 +1100

    Update

--- share/qt/make_windows_icon.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# create multiresolution windows icon
-ICON_DST=../../src/qt/res/icons/RAT.ico
-
-convert ../../src/qt/res/icons/RatCoin-16.png ../../src/qt/res/icons/RatCoin-32.png ../../src/qt/res/icons/RatCoin-48.png ${ICON_DST}
